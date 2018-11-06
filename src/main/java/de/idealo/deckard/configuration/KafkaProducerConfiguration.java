32260e298008deaa1692131af6812f76ed793050
roechi <msg.remus@gmail.com>
2018-11-06 10:34:24 +0100

    Boyscout - removes hard dependency on KafkaProperties
    - we noticed that expecting the Kafka Properties from the context may lead to unwanted interferences with smaller test contexts (like WebMvcTests)

--- src/main/java/de/idealo/deckard/configuration/KafkaProducerConfiguration.java
+++ src/main/java/de/idealo/deckard/configuration/KafkaProducerConfiguration.java
@@ -1,14 +1,18 @@
 package de.idealo.deckard.configuration;
 
-import java.util.Map;
-
 import de.idealo.deckard.proxy.ProxyBeanFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
 public class KafkaProducerConfiguration {
 
     @Bean
@@ -18,9 +22,16 @@ public class KafkaProducerConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    <K, V> KafkaTemplate<K, V> kafkaTemplate(KafkaProperties kafkaProperties) {
-        Map<String, Object> producerProps = kafkaProperties.buildProducerProperties();
+    <K, V> KafkaTemplate<K, V> kafkaTemplate(@Autowired(required = false) KafkaProperties kafkaProperties) {
+        KafkaProperties properties = Optional.ofNullable(kafkaProperties).orElseGet(() -> {
+            log.warn("You didn't specify any Kafka properties in your configuration. Either this is a test scenario," +
+                    "or this was not your intention.");
+            return new KafkaProperties();
+        });
+
+        Map<String, Object> producerProps = properties.buildProducerProperties();
         DefaultKafkaProducerFactory<K, V> producerFactory = new DefaultKafkaProducerFactory<>(producerProps);
+
         return new KafkaTemplate<>(producerFactory);
     }
 }
