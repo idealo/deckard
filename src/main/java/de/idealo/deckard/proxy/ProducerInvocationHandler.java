5adc59bbd86b894ca00aa8f65147f59bdea1f70e
marcus-j <mj@marcusjanke.de>
2019-01-10 16:21:38 +0100

    [UDS-1737] refrains from checking for method name

--- src/main/java/de/idealo/deckard/proxy/ProducerInvocationHandler.java
+++ src/main/java/de/idealo/deckard/proxy/ProducerInvocationHandler.java
@@ -1,13 +1,10 @@
 package de.idealo.deckard.proxy;
 
 import de.idealo.deckard.producer.Producer;
-import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-
-import static java.lang.String.format;
 
 @RequiredArgsConstructor
 public class ProducerInvocationHandler<K, V> implements InvocationHandler {
@@ -18,16 +15,12 @@ public class ProducerInvocationHandler<K, V> implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) {
-        if (!"send".equals(method.getName())) {
-            throw new IllegalArgumentException(format("Method %s does not exist.", method.getName()));
-        }
-
-        if (args.length == 1) {
-            producer.send((V) args[0]);
-        } else if (args.length == MAX_NUMBER_OF_ARGUMENTS) {
-            producer.send((K) args[0], (V) args[1]);
-        } else {
-            throw new IllegalArgumentException("Invalid number of arguments.");
+        if ("send".equals(method.getName())) {
+            if (args.length == 1) {
+                producer.send((V) args[0]);
+            } else if (args.length == MAX_NUMBER_OF_ARGUMENTS) {
+                producer.send((K) args[0], (V) args[1]);
+            }
         }
 
         return 0;
