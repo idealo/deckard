cf6fe9aa89e2624ea0fc0f48896c4d0f10d7475a
Richard Remus <richard.remus@idealo.de>
2019-12-06 17:15:35 +0100

    switches to fixed gpg key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,19 +8,8 @@ then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
 
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-
-    source .travis/gpg.sh
-
     mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease
 
-    if [ ! -z "$TRAVIS" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
